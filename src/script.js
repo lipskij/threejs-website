0b32b0ee9b2950e77b4d0eae35bf6149f12cfb77
Emil Lipskij <emil.lipskij@gmail.com>
2022-04-19 12:23:52 +0300

    feat: iamge with no icons

--- src/script.js
+++ src/script.js
@@ -232,10 +232,10 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Animate
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  // const elapsedTime = clock.getElapsedTime();
 
   // Update controls
   controls.update();
