5d726870fa9bfcbcc8e4ef684e80e07ee5727621
Emil Lipskij <emil.lipskij@gmail.com>
2022-07-18 11:31:57 +0300

    update home page button animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,57 @@ import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import Nav from "../components/Nav";
 import Loader from "../components/Loader";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
+  const text = {
+    hidden: {
+      opacity: 0,
+      y: -100,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
   return (
     <div>
       <main className={styles.main}>
         <Loader />
         <Nav />
         <section className={styles.home}>
-          <div className={styles.homeText}>
+          <motion.div
+            variants={text}
+            initial='hidden'
+            animate='visible'
+            transition={{
+              default: { duration: 1, ease: "easeInOut", delay: 1.5 },
+            }}
+            className={styles.homeText}
+          >
             <h1 className={styles.homeText1}>Hi, I&apos;m Emil.</h1>
             <h2 className={styles.homeText1}>
               I&apos;m a front-end web developer.
             </h2>
             <p>I create websites and web applications</p>
-            <button className={styles.homeBtn}>
+            <motion.button
+              animate={{
+                rotate: [0, -20, 0, 20, 0, -20, 0],
+                scale: [1, 1.2, 1, 0.8, 1],
+                boxShadow: "2px 2px 10px 2px #5bf1f6",
+                transition: {
+                  duration: 0.5,
+                  ease: "easeInOut",
+                  loop: Infinity,
+                  repeatDelay: 1,
+                  delay: 1,
+                },
+              }}
+              className={styles.homeBtn}
+            >
               <a href='#contacts'> Contact </a>
-            </button>
-          </div>
+            </motion.button>
+          </motion.div>
         </section>
       </main>
     </div>
